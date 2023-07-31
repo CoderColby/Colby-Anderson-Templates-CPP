6863207e48ac1cdde6e0ef19d42389fb67c1c714
CoderColby <97490678+CoderColby@users.noreply.github.com>
2023-07-31 16:37:04 +0000

    Created template function and integer function for half and test cases

--- .ccls-cache/@home@runner@Templates/main.cpp
+++ .ccls-cache/@home@runner@Templates/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+using namespace std;
+
+template <typename T>
+T half(T);
+int half(int);
 
 int main() {
-  std::cout << "Hello World!\n";
+  double a = 7.0;
+  float b = 5.0f;
+  int c = 3;
+
+  cout << half(a) << endl;
+  cout << half(b) << endl;
+  cout << half(c) << endl;
+}
+
+template <typename T>
+T half(T num) {
+  return num / 2;
+}
+
+int half(int num) {
+  return num / 2 + num % 2;
 }
