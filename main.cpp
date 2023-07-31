c5483e4aa8171e1628a17f64c39883f6d1f98547
CoderColby <97490678+CoderColby@users.noreply.github.com>
2023-07-31 16:43:51 +0000

    Added comments to explain code and moved the functions to above main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
-T half(T);
-int half(int);
-
-int main() {
-  double a = 7.0;
-  float b = 5.0f;
-  int c = 3;
-
-  cout << half(a) << endl;
-  cout << half(b) << endl;
-  cout << half(c) << endl;
-}
-
+// For doubles and floats that do not need rounding
 template <typename T>
 T half(T num) {
   return num / 2;
 }
 
+// For integers that need rounding
 int half(int num) {
-  return num / 2 + num % 2;
+  return num / 2 + num % 2; // I add 1 if 'num' is odd to simulate rounding since integer division always rounds down
+}
+
+int main() {
+  // Test cases
+  double a = 7.0; // Change me
+  float b = 5.0f; // Change me
+  int c = 3; // Change me
+
+  cout << half(a) << endl;
+  cout << half(b) << endl;
+  cout << half(c) << endl;
 }
